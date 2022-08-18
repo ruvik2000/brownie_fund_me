1c5183c75853d9b80403d4369e6c281b14b8e897
Ruvik  Perera <75002655+ruvik2000@users.noreply.github.com>
2022-08-19 00:01:42 +0530

    Update deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -9,8 +9,6 @@ from scripts.helpful_scripts import get_account, deploy_mocks, LOCAL_BLOCKCHAIN_
 
 def deploy_fund_me():
     account = get_account()
-    # pass the price feed address to our fundme contract
-
     #if we are on a presistent network like rinkeby, use the associated address, otherwise deploy mocks
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
@@ -31,7 +29,5 @@ def deploy_fund_me():
 #         return accounts[0]
 #     else:
 #         return accounts.add(config["wallets"]["from_key"]) 
-
-
 def main():
     deploy_fund_me()
